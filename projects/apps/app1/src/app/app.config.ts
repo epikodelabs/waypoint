8ec1e909a37a67f68dbfdf17067a1f29e0b21c1f
actioncrew <sepeloleksij4@gmail.com>
2026-07-31 20:31:38 +0300

    application component is standalone

--- projects/apps/app1/src/app/app.config.ts
+++ projects/apps/app1/src/app/app.config.ts
@@ -1,13 +1,17 @@
 import {
   ApplicationConfig,
+  ApplicationModule,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { provideStreamixRouter } from '@epikodelabs/waypoint';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(ApplicationModule, BrowserModule),
     provideBrowserGlobalErrorListeners(),
     ...provideStreamixRouter(routes, {
       viewTransitions: true,
--- projects/apps/app1/src/app/app.ts
+++ projects/apps/app1/src/app/app.ts
@@ -6,6 +6,7 @@ import {
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     RouterOutlet,
     StreamixRouterLink,
